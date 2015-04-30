51f61a2bee3b25eb6523fdc5a658c74a4ed29c80
Ellisande <justin.dragos@gmail.com>
2015-04-30 13:30:03 -0700

    Attempted to add swagger as an api engine.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
+var swagger = require("swagger-node-express").createNew(app);
 app.use(bodyParser.json());
+app.use('/apis', express.static('node_modules/swagger-ui/dist'));
 
 // Sets up an convience method for requiring local modules.
 global.localRequire = function(moduleName){
@@ -16,10 +18,15 @@ var lookupEndpoint = localRequire('apis/endpoint/lookup');
 var updateEndpoints = localRequire('apis/endpoint/update');
 var createApplication = localRequire('apis/application/create');
 var addApplicationVersion = localRequire('apis/application/version');
+ 
 
-app.post('/contract/add', function (req, res) {
-  addContract.action(req, res);
-});
+swagger.addModels(localRequire('model/app-model'));
+
+swagger.addPost(addContract);
+
+// app.post('/contract/add', function (req, res) {
+//   addContract.action(req, res);
+// });
 
 app.post('/contract/remove', function(req, res){
   removeContract.action(req, res);
@@ -44,6 +51,9 @@ app.post('/application/create', function(req, res){
 app.post('/endpoint/update', function(req, res){
   updateEndpoints.action(req, res);
 });
+
+swagger.configureSwaggerPaths("", "/api-docs", "");
+swagger.configure("http://localhost:3000", "0.1");
 
 //Turn on the server and listen.
 var server = app.listen(3000, function () {
