9737b9d53e253b3292026332dc9f8793e3d27d64
Ellisande <justin.dragos@gmail.com>
2015-04-30 13:29:18 -0700

    Attempting to add a swagger spec to the contract add.

--- apis/contract/add.js
+++ apis/contract/add.js
@@ -2,11 +2,26 @@ var registry = require('../../database/registry');
 var contracts = require('../../database/contracts');
 var versionUtils = localRequire('utils/version');
 var uuid = localRequire('utils/uuid');
+var swagger = require('swagger-node-express');
 
-var spec = {}
+var sampleResponse = {"chat-service-2.0.3":{"api_key":"4040384b-79b7-4b19-bb8a-1364d5ea5ac7","provider_name":"chat-service","provider_version":"2.0.3","consumer_name":"epe","consumer_version":"1.0.0"}}
+
+var spec = {
+  "description" : "Creates a new contract between two application versions.",
+  "path" : "/contract/add",
+  "notes" : "Adds a contract between the consumer version and the provider version.",
+  "summary" : "Add a contract",
+  "method": "POST",
+  "parameters" : [swagger.paramTypes.body("Contract", "object describing the name and version of the consumer for the contract", 'Contract', sampleResponse, true)],
+  "type" : "Contract",
+  "responseMessages" : [swagger.errors.notFound('provider_version')],
+  "nickname" : "addContract"
+}
+
 
 var addContract = function(req, res){
   var consumer = req.body.consumer;
+  console.log(req.body);
   var requestedProvider = req.body.provider;
 
   registry.findOne({name: requestedProvider.name}, function(err, provider){
