37a807e56af653fc40163ae078a4680464ac247e
Ellisande <justin.dragos@gmail.com>
2015-04-30 13:29:50 -0700

    Attempted to create a swagger model.

---
+++ model/app-model.js
@@ -0,0 +1,7 @@
+var contractModel = localRequire('model/contract-model');
+var registryModel = localRequire('model/registry-model');
+
+module.exports = {
+  "Contract": contractModel,
+  "Registry": registryModel
+}
