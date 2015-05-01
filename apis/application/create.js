876e3462cc7e4ae5470248279a7c4107040a3793
Ellisande <justin.dragos@gmail.com>
2015-04-30 17:17:15 -0700

    Modified create so that it will update or create as necessary.

--- apis/application/create.js
+++ apis/application/create.js
@@ -1,21 +1,47 @@
 var registry = localRequire('database/registry');
 var registryValidator = localRequire('registry');
+var _ = require('lodash');
+var validOptions = localRequire('models/options');
 
 var spec = {}
 
 var createApplication = function(req, res){
-  var newApplication = req.body;
-  registry.findOne({name: newApplication.name}, function(application){
-    if(application){
-      return res.status(400).send("An application named " + newApplication.name + " already exists.")
+  var newApplication = _.pick(req.body, ['name', 'options']);
+  if(_.isEmpty(newApplication.name)){
+    return res.status(400).send("You must provide a name for the application you wish to create.");
+  }
+  if(!newApplication.options){
+    newApplication.options = {};
+  }
+  newApplication.options = _.pick(newApplication.options, validOptions);
+  registry.findOne({name: newApplication.name}, function(err, data){
+    if(err){
+      return res.status(400).send("The application you tried to create was not valid");
     }
-
-    if(registryValidator(newApplication)){
-      registry.insert(newApplication);
+    if(!data){
+      newApplication.versions = [];
+      create(newApplication);
+      if(newApplication._id){
+        delete(newApplication._id);
+      }
       return res.status(200).send(newApplication);
     }
+    update(newApplication, res);
+  });
+}
 
-    return res.status(400).send("The application you tried to create was not valid");
+var create = function(newApplication){
+  registry.insert(newApplication);
+}
+
+var update = function(newApplication, res){
+  registry.update({name: newApplication.name}, {$set: {options: newApplication.options}});
+  registry.findOne({name: newApplication.name}, function(err, data){
+    if(data){
+      delete(data._id);
+      return res.send(data);
+    }
+    return res.status(500).send('Internal service error');
   });
 }
 
